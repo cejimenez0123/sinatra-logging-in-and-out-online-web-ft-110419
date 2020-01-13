2d0f298ea3c206533fd4f13bff121196800fab6b
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-13 17:34:14 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User
+  attr_accessor :username, :password, :balance
+  def initialize(hash)
+    @username = hash[:username]
+    @password = hash[:password]
+    @balance = hash[:balance]
+  end
+  def self.create(hash)
+    self.new(hash)
+  end
 
 end
