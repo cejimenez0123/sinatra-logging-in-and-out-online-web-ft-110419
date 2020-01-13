791dcac52914cc0c8f9e4a98fa00605fad9e7aec
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-13 17:32:38 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,10 @@
 class Helpers
-  def self.current_user(session)
-   User.find_by(id: session[:user_id])
- end
-
- def self.is_logged_in?(session)
-   session[:user_id].is_a? Integer
- end
+  
+    def self.current_user(session)
+      @user = User.find_by_id(session[:user_id])
+    end
+  
+    def self.is_logged_in?(session)
+      !!session[:user_id]
+    end
 end
