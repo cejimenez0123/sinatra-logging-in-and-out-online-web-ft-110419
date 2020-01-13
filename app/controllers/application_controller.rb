c44e889a111471318fcab57643a4421f0b0bd914
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-13 17:54:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,26 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
-
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
-
 end
-
